48aed313f0d1a061dd7ea491451994d9e5b6af75
athena272 <guilhermera272@gmail.com>
2022-09-24 16:39:35 -0300

    Create GameStartTest.java

---
+++ src/com/teste/GameStartTest.java
@@ -0,0 +1,12 @@
+package com.teste;
+
+import com.main.grupo.Game;
+import org.junit.Test;
+
+public class GameStartTest {
+    @Test
+    public void Game() {
+        Game game = new Game();
+        game.play();
+    }
+}
