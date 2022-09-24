273170b7cab068b8d845d4bdd561d5ac19665cae
athena272 <guilhermera272@gmail.com>
2022-09-24 16:50:12 -0300

    Create GameTest.java

---
+++ src/com/teste/GameTest.java
@@ -0,0 +1,20 @@
+package com.teste;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void play() {
+    }
+}
