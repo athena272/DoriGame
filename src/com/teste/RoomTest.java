98ae32e3f39a3522cb855382e29143fb3b81873c
Cauan Santos <cauansantos043@gmail.com>
2022-09-24 17:17:21 -0300

    Create RoomTest.java

---
+++ src/com/teste/RoomTest.java
@@ -0,0 +1,26 @@
+package com.teste;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setExit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getShortDescription() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLongDescription() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getExit() {
+    }
+}
