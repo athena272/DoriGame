d36ecacf16f1e89661da0d4699acdc6a91c1fa0a
athena272 <guilhermera272@gmail.com>
2022-09-24 16:57:46 -0300

    Create ParserTest.java

---
+++ src/com/teste/ParserTest.java
@@ -0,0 +1,16 @@
+package com.teste;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @Test
+    void getCommand() {
+    }
+
+    @Test
+    void showCommands() {
+    }
+}
