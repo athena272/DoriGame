a510c07aa91520f7b510b58caa89493be67a9e65
athena272 <guilhermera272@gmail.com>
2022-09-24 16:57:44 -0300

    Create CommandWordTest.java

---
+++ src/com/teste/CommandWordTest.java
@@ -0,0 +1,64 @@
+package com.teste;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandWordTest {
+
+    @Test
+    void name() {
+    }
+
+    @Test
+    void ordinal() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testClone() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void getDeclaringClass() {
+    }
+
+    @Test
+    void describeConstable() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+
+    @Test
+    void testFinalize() {
+    }
+
+    @Test
+    void testToString1() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void testValueOf() {
+    }
+}
