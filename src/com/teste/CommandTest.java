dcd3ed90026c39f1de91492e76643ab9d42eb4a6
athena272 <guilhermera272@gmail.com>
2022-09-24 16:39:33 -0300

    Create CommandTest.java

---
+++ src/com/teste/CommandTest.java
@@ -0,0 +1,25 @@
+package com.teste;
+
+import org.junit.Test;
+
+public class CommandTest {
+    @Test
+    public void testGetCommandWord() {
+
+    }
+
+    @Test
+    public void testGetSecondWord() {
+
+    }
+
+    @Test
+    public void testHasSecondWord() {
+
+    }
+
+    @Test
+    public void testIsUnknown() {
+
+    }
+}
